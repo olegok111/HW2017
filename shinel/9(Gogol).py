6b440673f44d57896d6a4448a4339224c7338b80
olegok111 <olegok2003@mail.ru>
2017-04-07 23:26:17 +0300

    Deleted shinel

--- shinel/9(Gogol).py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-import re
-with open('Shinel.txt', 'r') as text:
-    text_encoded = text.readlines()
-    text_encoded1 = str(text_encoded)
-    print(len(re.findall('[^ ]+', text_encoded1)))
